89a7b9b99b1136327e7b776613f301d006f15a1a
Tirthankar Bhattacharjee <feignfroggie@gmail.com>
2015-04-06 00:19:10 -0400

    add template form, and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,46 @@
 class PostsController < ApplicationController
 
   def index
-   @posts = Post.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-    post = Post.new(post_params)
-    post.save
+    @post = Post.new(post_params)
 
-    redirect_to post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+
+    redirect_to root_path
   end
 
   private
